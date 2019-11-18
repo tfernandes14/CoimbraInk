4e653905354a10cc5c3f1e585ff122727c83c797
hugoredinho <hugoredinho@gmail.com>
2019-11-18 00:56:15 +0000

    Fundo webresponsive e bases de dados comentada no settings.py

---
+++ website/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+
+def home(request):
+    return render(request, 'base.html')
