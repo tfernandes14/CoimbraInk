2ff1c38f49bdf13efd65f1e29d8a20163e59dfde
Tiago <fernandes.tiago36@gmail.com>
2019-11-23 23:40:31 +0000

    Menu de admin tratado, mas continuo com o problema no html

--- website/models.py
+++ website/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Reprografia(models.Model):
-    nome = models.TextField()
+    nome = models.CharField(max_length=50)
     latitude = models.FloatField()
     longitude = models.FloatField()
     rating_medio = models.FloatField(default=0.0)
@@ -17,12 +17,15 @@ class Reprografia(models.Model):
     horario_fecho_fds_tarde = models.FloatField()     # 9.30 ==> 9h30
     cartao_visita_preco = models.FloatField(default=-1.0)
 
+    def __str__(self):
+        return self.nome
+
 
 class A5(models.Model):
     reprografia = models.OneToOneField(
         Reprografia,
         on_delete=models.CASCADE,
-        primary_key=True
+        primary_key=True,
     )
     cor_preco = models.FloatField(default=-1.0)
     preto_branco_preco = models.FloatField(default=-1.0)
