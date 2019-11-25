66c67857cee046003be5d330039d133c2d8af021
Tiago <fernandes.tiago36@gmail.com>
2019-11-25 22:04:44 +0000

    Apenas mostra na localização do primeiro ponto dado, mas depois nao mete marcadores para os restantes pontos

--- website/views.py
+++ website/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib import messages
+from django.shortcuts import render
+from .models import *
+
 
 def home(request):
-    return render(request, 'base.html')
+    todas = get_lat_long()
+    return render(request, 'base.html', {'lista': todas})
+
+
+def get_lat_long():
+    res = []
+    tam_reprografias = len(list(Reprografia.objects.all()))
+    for i in range(1, tam_reprografias + 1):
+        rep = Reprografia.objects.get(id=i)
+        aux = [rep.nome, rep.latitude, rep.longitude]
+        res.append(aux)
+    return res
