519ca4389e70d61dc277639e8b9368df2e5b0810
Tiago <fernandes.tiago36@gmail.com>
2019-11-26 01:24:09 +0000

    Ja mete todas as reprografias da BD no mapa dinâmico e com uma mensagem para saber qual é, mas ainda n saca a localização

--- website/views.py
+++ website/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from .models import *
 
 
 def home(request):
     todas = get_lat_long()
+    print(todas)
     return render(request, 'base.html', {'lista': todas})
 
 
 def get_lat_long():
     res = []
     tam_reprografias = len(list(Reprografia.objects.all()))
-    for i in range(1, tam_reprografias + 1):
-        rep = Reprografia.objects.get(id=i)
-        aux = [rep.nome, rep.latitude, rep.longitude]
-        res.append(aux)
+    print("&&&&", tam_reprografias)
+    for i in range(1, tam_reprografias + 2):
+        try:
+            rep = Reprografia.objects.get(id=i)
+        except ObjectDoesNotExist:
+            rep = None
+        if rep is not None:
+            aux = [rep.nome, rep.latitude, rep.longitude]
+            res.append(aux)
     return res
