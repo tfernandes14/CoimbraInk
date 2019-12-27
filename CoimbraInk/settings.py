e37692626810674b10774a64b99bf25b0f32db39
mcarolina5 <mcarolina5@users.noreply.github.com>
2019-12-27 23:24:16 +0000

    Update settings.py

--- CoimbraInk/settings.py
+++ CoimbraInk/settings.py
@@ -77,8 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'CoimbraInk',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
         'HOST': 'localhost'
     }
 }
